f624511c35e63b0d5ff90f1cfa9ba8a6688fad7a
Sarah Lee <sarahselee223@gmail.com>
2019-04-02 19:49:22 -0700

    add index image and icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,47 @@
 import React from "react"
-import { Link } from "gatsby"
 import styles from '../styles/index.module.css'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import robot1 from '../images/robot1.jpeg'
+import githubLogo from '../images/github-logo.png'
+import linkedinLogo from '../images/linkedin-logo-button.png'
+import emailLogo from '../images/email.png'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1 className={styles.test}>Hi people</h1>
-    <p className={styles.test}>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div className={styles.indexMargin}>
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <pattern id="img" patternUnits="userSpaceOnUse" width="100" height="100">
+              <image href={robot1} alt="robot" x="-25" width="150" height="100" />
+            </pattern>
+          </defs>
+          <polygon points="50 1 95 25 95 75 50 99 5 75 5 25" fill="url(#img)" stroke="black" stroke-width="0.2%"/>
+        </svg>
+      </div>
+      <div className={styles.myName}>
+        <span>Sarah S. Lee</span>
+      </div>
+      <div className={styles.myKoreanName}>
+        <span>이 승 은</span>
+      </div>
+      <div className={styles.myTitle}>
+        <span>Software Engineer</span>
+      </div>
+      <div>
+        <span className={styles.iconContainer}>
+          <img src={githubLogo} alt="githubLogo" />
+        </span>
+        <span className={styles.iconContainer}>
+          <img src={linkedinLogo} alt="linkedinLogo" />
+        </span>
+        <span className={styles.iconContainer}>
+          <img src={emailLogo} alt="emailLogo" />
+        </span>
+      </div>
     </div>
-    <Link to="/aboutMe/">AboutMe</Link>
   </Layout>
 )
 
