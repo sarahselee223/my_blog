16d9609503edaac4b14e0ece79a84e5baaff5087
Sarah Lee <sarahselee223@gmail.com>
2019-05-13 16:10:41 -0700

    update json categories

--- src/components/project.js
+++ src/components/project.js
@@ -1,18 +1,23 @@
 import React from "react"
 import Image from "gatsby-image"
+import styles from '../styles/project.module.css'
 
-const Project = ({ title, description, price, image }) => {
+const Project = ({ title, subtitle, description, takeAway, technology, demo, github, image }) => {
 
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className={styles.container}>
+      <h3 className={styles.title}>{title}</h3>
       <Image
         fluid={image.childImageSharp.fluid}
         alt={title}
-        style={{ float: "left", marginRight: "1rem", width: 150 }}
+        style={{ float: "left", marginRight: "1rem", width: 250 }}
       />
-      <p>{price}</p>
-      <div dangerouslySetInnerHTML={{ __html: description }} />
+      <p className={styles.subtitle}>{subtitle}</p>
+      <div dangerouslySetInnerHTML={{ __html: description }} className={styles.description}/>
+      <p className={styles.takeAway}>{takeAway}</p>
+      <p className={styles.technology}>{technology}</p>
+      <a href="{https://youtu.be/-YDvsXGxs_Y}" className={styles.demo}>{demo}</a>
+      <p className={styles.github}>{github}</p>
       <div style={{ clear: 'both' }}></div>
     </div>
   )
